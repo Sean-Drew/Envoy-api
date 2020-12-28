7ad0fc2a80a178a865a1d97012cf5af460944354
Sean-Drew <SDrew989@gmail.com>
2020-12-28 11:34:14 -0500

    Modify config variables

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'group-project-api'
+const mongooseBaseName = 'envoy-api'
 
 // create the mongodb uri for development and test
 const database = {
